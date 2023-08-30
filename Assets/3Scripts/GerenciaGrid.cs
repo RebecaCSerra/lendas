c0f1e5e7d4a997b0fdc476852eeb30a090526ce4
RebecaCSerra <rebeca.serra@acad.ifma.edu.br>
2023-08-30 11:10:25 -0300

    30/8

--- Assets/3Scripts/GerenciaGrid.cs
+++ Assets/3Scripts/GerenciaGrid.cs
@@ -1,9 +1,8 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
 
 
 public class GerenciaGrid : MonoBehaviour
@@ -20,20 +19,19 @@ public class GerenciaGrid : MonoBehaviour
 
     public int StartingMoves = 10;
     private int _numMoves;
-    private int CountMoves = 0;
-    private int CountMatches;
+    private int CountMoves;
     public int NumMoves
     {
-      get
-      {
-         return _numMoves;
-     }
-
-      set
-      {
-         _numMoves = value;
+        get
+        {
+            return _numMoves;
         }
-      }
+
+        set
+        {
+            _numMoves = value;
+        }
+    }
     public int LevelGoal1 = 5;
     public int LevelGoal2 = 5;
     private int _numTiles;
@@ -54,15 +52,15 @@ public class GerenciaGrid : MonoBehaviour
     void Awake()
 
     {
-       Instance = this;
-       NumMoves = StartingMoves;
+        Instance = this;
+        NumMoves = StartingMoves;
     }
 
     // Start is called before the first frame update
     void Update()
     {
-        if(NumMoves >0)
-        MovesText.text = NumMoves.ToString();
+        if (NumMoves > 0)
+            MovesText.text = NumMoves.ToString();
         else
             SceneManager.LoadScene(8);
 
@@ -76,12 +74,12 @@ public class GerenciaGrid : MonoBehaviour
     void InitGrid()
     {
         Vector3 positionOffset = transform.position - new Vector3(GridDimension * Distance / 2.0f, GridDimension * Distance / 2.0f, 0);
-       
-            for (int row = 0; row < GridDimension; row++)
+
+        for (int row = 0; row < GridDimension; row++)
             for (int column = 0; column < GridDimension; column++)
             {
                 GameObject newTile = Instantiate(TilePrefab);
-              
+
                 List<Sprite> possibleSprites = new List<Sprite>(Sprites);
 
                 //Choose what sprite to use for this cell
@@ -100,7 +98,7 @@ public class GerenciaGrid : MonoBehaviour
                 }
 
                 SpriteRenderer renderer = newTile.GetComponent<SpriteRenderer>();
-                renderer.sprite = possibleSprites[UnityEngine.Random.Range(0, possibleSprites.Count)];
+                renderer.sprite = possibleSprites[Random.Range(0, possibleSprites.Count)];
 
                 Tile tile = newTile.AddComponent<Tile>();
                 tile.Position = new Vector2Int(column, row);
@@ -108,7 +106,7 @@ public class GerenciaGrid : MonoBehaviour
                 newTile.transform.parent = transform;
                 newTile.transform.position = new Vector3(column * Distance, row * Distance, 0) + positionOffset;
                 newTile.name = renderer.sprite.name;
-                
+
                 Grid[column, row] = newTile;
             }
     }
@@ -146,70 +144,64 @@ public class GerenciaGrid : MonoBehaviour
         renderer2.sprite = temp;
 
         bool changesOccurs = CheckandCountMatches();
-        
         if (!changesOccurs)
         {
             temp = renderer1.sprite;
             renderer1.sprite = renderer2.sprite;
             renderer2.sprite = temp;
             //GerenciaSom.Instance.PlaySound(SoundType.TypeMove);
-           
+
         }
         else
         {
             //GerenciaSom.Instance.PlaySound(SoundType.TypePop);
-            //teve mudança
-            print(CountMoves + "countmoves");
-            if (CountMoves>=2)
-            {
-                int CountGoal1 = int.Parse(LevelGoal1Text.text) - (CountMoves + 1);
-                if (CountGoal1 >= 0)
-                    LevelGoal1Text.text = CountGoal1.ToString();
-              
-            }
+            //teve mudanï¿½a
+            print("Count" + CountMoves);
+            int CountGoal1 = int.Parse(LevelGoal1Text.text) - CountMoves;
+            if (CountGoal1 >= 0)
+                LevelGoal1Text.text = CountGoal1.ToString();
 
-            CountMoves = 0;
             NumMoves--;
-            //do
-            //{
-            //    FillHoles();
-            //} while (CheckMatches());
-            // if (NumMoves <= 0)
-            //{
-            //       NumMoves = 0;            
-            //}
+            do
+            {
+                FillHoles();
+            } while (CheckMatches());
+            if (NumMoves <= 0)
+            {
+                NumMoves = 0;
+            }
+        }
+    }
+
+    bool CheckMatches()
+    {
+        HashSet<SpriteRenderer> matchedTiles = new HashSet<SpriteRenderer>();
+        for (int row = 0; row < GridDimension; row++)
+        {
+            for (int column = 0; column < GridDimension; column++)
+            {
+                SpriteRenderer current = GetSpriteRendererAt(column, row);
+
+                List<SpriteRenderer> horizontalMatches = FindColumnMatchForTile(column, row, current.sprite);
+                if (horizontalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(horizontalMatches);
+                    matchedTiles.Add(current);
+                }
+
+                List<SpriteRenderer> verticalMatches = FindRowMatchForTile(column, row, current.sprite);
+                if (verticalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(verticalMatches);
+                    matchedTiles.Add(current);
+                }
             }
         }
 
-        bool CheckMatches()
+        foreach (SpriteRenderer renderer in matchedTiles)
         {
-            HashSet<SpriteRenderer> matchedTiles = new HashSet<SpriteRenderer>();
-            for (int row = 0; row < GridDimension; row++)
-            {
-                for (int column = 0; column < GridDimension; column++)
-                {
-                    SpriteRenderer current = GetSpriteRendererAt(column, row);
-
-                    List<SpriteRenderer> horizontalMatches = FindColumnMatchForTile(column, row, current.sprite);
-                    if (horizontalMatches.Count >= 2)
-                    {
-                        matchedTiles.UnionWith(horizontalMatches);
-                        matchedTiles.Add(current);
-                    }
-
-                    List<SpriteRenderer> verticalMatches = FindRowMatchForTile(column, row, current.sprite);
-                    if (verticalMatches.Count >= 2)
-                    {
-                        matchedTiles.UnionWith(verticalMatches);
-                        matchedTiles.Add(current);
-                    }
-                }
-            }
-
-            foreach (SpriteRenderer renderer in matchedTiles)
-            {
             //renderer.color = Color.red;
-            
+            print(renderer.name + "em match");
             renderer.sprite = null;
 
             bool hasmatch = matchedTiles.Count > 0;
@@ -223,10 +215,10 @@ public class GerenciaGrid : MonoBehaviour
             //    print("deu match" + matchedTiles.Count);
             //    LevelGoal2Text.text = CountGoal2.ToString();
             //}
-               
+
             //if (CountGoal2 >= 0)
             //    LevelGoal2Text.text = CountGoal2.ToString();
-            
+
 
         }
 
@@ -260,70 +252,16 @@ public class GerenciaGrid : MonoBehaviour
         foreach (SpriteRenderer renderer in matchedTiles)
         {
             //renderer.color = Color.red;
-            
+
             renderer.sprite = null;
 
             bool hasmatch = matchedTiles.Count > 0;
             int CountGoal2 = 0;
-             print(renderer.name + " em match " + CountMoves + " match " + matchedTiles.Count);
-            if (String.Equals(renderer.name, "fogo-removebg-preview"))
+
+            if (renderer.name == "fogo-removebg-preview")
             {
-               
-                CountMoves++;
-                CountMatches = matchedTiles.Count;
-                //CountMoves = matchedTiles.Count;
-            }
-
-            //if (CountGoal2 >= 0)
-            //    LevelGoal2Text.text = CountGoal2.ToString();
-
-
-        }
-
-        return matchedTiles.Count > 0;
-    }
-
-
-    bool CheckandCountMatches(string name)
-    {
-        HashSet<SpriteRenderer> matchedTiles = new HashSet<SpriteRenderer>();
-        for (int row = 0; row < GridDimension; row++)
-        {
-            for (int column = 0; column < GridDimension; column++)
-            {
-                SpriteRenderer current = GetSpriteRendererAt(column, row);
-                if (!String.Equals(current.name, name)) break;
-                    List<SpriteRenderer> horizontalMatches = FindColumnMatchForTile(column, row, current.sprite);
-                if (horizontalMatches.Count >= 2)
-                {
-                    matchedTiles.UnionWith(horizontalMatches);
-                    matchedTiles.Add(current);
-                }
-
-                List<SpriteRenderer> verticalMatches = FindRowMatchForTile(column, row, current.sprite);
-                if (verticalMatches.Count >= 2)
-                {
-                    matchedTiles.UnionWith(verticalMatches);
-                    matchedTiles.Add(current);
-                }
-            }
-        }
-
-        foreach (SpriteRenderer renderer in matchedTiles)
-        {
-            //renderer.color = Color.red;
-
-            //renderer.sprite = null;
-
-            bool hasmatch = matchedTiles.Count > 0;
-            int CountGoal2 = 0;
-
-            if (String.Equals(renderer.name, "fogo-removebg-preview"))
-            {
-                print(renderer.name + " em match " + CountMoves + " match " + matchedTiles.Count);
-                CountMoves++;
-                CountMatches = matchedTiles.Count;
-                //CountMoves = matchedTiles.Count;
+                print(renderer.name + "em match");
+                CountMoves = matchedTiles.Count;
             }
 
             //if (CountGoal2 >= 0)
@@ -335,34 +273,34 @@ public class GerenciaGrid : MonoBehaviour
         return matchedTiles.Count > 0;
     }
     List<SpriteRenderer> FindColumnMatchForTile(int col, int row, Sprite sprite)
+    {
+        List<SpriteRenderer> result = new List<SpriteRenderer>();
+        for (int i = col + 1; i < GridDimension; i++)
         {
-            List<SpriteRenderer> result = new List<SpriteRenderer>();
-            for (int i = col + 1; i < GridDimension; i++)
+            SpriteRenderer nextColumn = GetSpriteRendererAt(i, row);
+            if (nextColumn.sprite != sprite)
             {
-                SpriteRenderer nextColumn = GetSpriteRendererAt(i, row);
-                if (nextColumn.sprite != sprite)
-                {
-                    break;
-                }
-                result.Add(nextColumn);
+                break;
             }
-            return result;
+            result.Add(nextColumn);
         }
+        return result;
+    }
 
-        List<SpriteRenderer> FindRowMatchForTile(int col, int row, Sprite sprite)
+    List<SpriteRenderer> FindRowMatchForTile(int col, int row, Sprite sprite)
+    {
+        List<SpriteRenderer> result = new List<SpriteRenderer>();
+        for (int i = row + 1; i < GridDimension; i++)
         {
-            List<SpriteRenderer> result = new List<SpriteRenderer>();
-            for (int i = row + 1; i < GridDimension; i++)
-            {
             SpriteRenderer nextRow = GetSpriteRendererAt(col, i);
-                if (nextRow.sprite != sprite)
-                {
-                    break;
-                }
-                result.Add(nextRow);
+            if (nextRow.sprite != sprite)
+            {
+                break;
             }
-            return result;
+            result.Add(nextRow);
         }
+        return result;
+    }
     void FillHoles()
     {
         for (int column = 0; column < GridDimension; column++)
@@ -373,7 +311,7 @@ public class GerenciaGrid : MonoBehaviour
                     SpriteRenderer current = GetSpriteRendererAt(column, row);
 
 
-                    current.sprite = Sprites[UnityEngine.Random.Range(0, Sprites.Count)];
+                    current.sprite = Sprites[Random.Range(0, Sprites.Count)];
                 }
             }
     }
